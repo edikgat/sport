83737551d0e856e6f89e8df3101996d36fd33fb9
edikgat <edikgat@gmail.com>
2012-08-08 17:11:35 +0400

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :birth_date, :description, :email, :first_name, 
+  :last_name,:email, :password, :password_confirmation,
+  :remember_me
+  # attr_accessible :title, :body
+ 
+
+######################
+
+  ##microposts
+  #has_many :microposts, :dependent => :destroy
+  ##events
+  #has_many :users_events
+  #has_many :events, :through => :users_events
+
+#  #messages
+#  has_many :messages, :dependent => :destroy,
+#                           :foreign_key => "sender_id" #исх сообщения
+#  has_many :reverse_messages, :dependent => :destroy,
+#                                   :foreign_key => "receiver_id", 
+#                                   :class_name => "Message" #вх сообщения#
+#
+#  has_many :receivers, :through => :messages, :source => :receiver
+#  has_many :senders, :through => :reverse_messages,
+#                       :source => :sender
+
+##ish zayavka
+
+#  has_many :friendships
+#  has_many :friends, :through => :friendships
+#  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+#  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+#  has_many :authorized_friends, :through => :friendships, :source => :friend, :conditions => [ "authorized = ?", true ]
+
+####################
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sport::Application.config.secret_token = '38249a372e21d248cee024e26235fd84cc02c3e99bc8dadd399ee99decfd94907ff40cf12882fce6fc8c2c555eee1c88a9afea3b11838e625388819839d8a893'
